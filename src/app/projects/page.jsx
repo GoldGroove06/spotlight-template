88f1d7af710c04a1e2fe3805ef18a8d30d362eb0
GoldGroove06 <73437174+GoldGroove06@users.noreply.github.com>
2025-06-03 14:31:15 +0530

    project page + card in WIP

---
+++ src/app/projects/page.jsx
@@ -0,0 +1,29 @@
+"use client"
+import React from 'react';
+import Heading from "@radui/ui/Heading";
+import Text from "@radui/ui/Text";
+import Seperator from "@radui/ui/Separator";
+import Card from "@/components/Card"
+import ProjectCard from '../../components/ProjectCard';
+
+function page(props) {
+    return (
+        <div className="flex flex-col items-center bg-gray-100 justify-center min-h-screen text-gray-1000">
+      <div className="bg-[var(--rad-ui-color-gray-300)] h-full justify-center items-start flex flex-col lg:w-[90vw] w-screen border border-l-gray-600 border-r-gray-600 p-16 space-y-8">
+            <section className='max-w-3xl space-y-8 mb-16'>
+            <Heading as="h1">Things I’ve made trying to put my dent in the universe.</Heading>
+            <Text className="text-slate-900 !text-lg">
+                I’ve worked on tons of little projects over the years but these are the ones that I’m most proud of. Many of them are open-source, so if you see something that piques your interest, check out the code and contribute if you have ideas for how it can be improved.
+            </Text>
+        </section>
+            <section className='flex flex-col w-[80%] '>
+               
+                <ProjectCard name="name" content="content" link="link" />
+            </section>
+
+        </div>
+        </div>
+    );
+}
+
+export default page;
---
+++ src/components/ProjectCard.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import Heading from "@radui/ui/Heading";
+import Text from "@radui/ui/Text";
+import Link from 'next/link';
+import Avatar from "@radui/ui/Avatar";
+
+function ProjectCard({name, content, link}) {
+    return (
+        <div className='p-8 space-y-6 hover:bg-gray-400 hover:bg-opacity-70 rounded-2xl hover:text-teal-900'>
+            
+               <span className='h-12 w-14 items-center justify-center flex bg-slate-200 rounded-[100%]'>
+            <Avatar.Root>
+              <Avatar.Image src="https://i.pravatar.cc/1000" />
+              <Avatar.Fallback>KL</Avatar.Fallback>
+            </Avatar.Root>
+            </span> 
+        
+            <Heading as="h6" className="text-gray-1000">{name}</Heading>
+            <Text className="text-slate-900 text-sm">{content}</Text>
+            <Link href={link} >{link}</Link>
+        </div>
+    );
+}
+
+export default ProjectCard;
