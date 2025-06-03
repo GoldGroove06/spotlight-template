931d474064bd05ad3f7a5693dda4f3e33c5d09ac
GoldGroove06 <73437174+GoldGroove06@users.noreply.github.com>
2025-06-03 14:21:34 +0530

    article page

---
+++ src/app/articles/page.jsx
@@ -0,0 +1,42 @@
+"use client"
+import React from 'react';
+import Heading from "@radui/ui/Heading";
+import Text from "@radui/ui/Text";
+import Seperator from "@radui/ui/Separator";
+import Card from "@/components/Card"
+
+function page(props) {
+    return (
+        <div className="flex flex-col items-center bg-gray-100 justify-center min-h-screen text-gray-1000">
+      <div className="bg-[var(--rad-ui-color-gray-300)] h-full justify-center items-start flex flex-col lg:w-[90vw] w-screen border border-l-gray-600 border-r-gray-600 p-16 space-y-8">
+            <section className='max-w-3xl space-y-8 mb-16'>
+            <Heading as="h1">Writing on software design, company building, and the aerospace industry.</Heading>
+            <Text className="text-slate-900 !text-lg">
+                All of my long-form thoughts on programming, leadership, product design, and more, collected in chronological order.
+            </Text>
+        </section>
+            <section className='flex flex-col w-[80%] '>
+               
+                <div className='flex flex-row space-x-4 items-start'>
+                     <Seperator orientation="vertical" className=""/>
+                    <div className='text-xs text-slate-800 mt-9 text-nowrap'>September 5, 2022</div>
+                <Card date="" title="Card title" description="Most companies try to stay ahead of the curve when it comes to visual design, but for Planetaria we needed to create a brand that would still inspire us 100 years from now when humanity has spread across our entire solar system." />
+                </div>
+                <div className='flex flex-row space-x-4 items-start'>
+                    <Seperator orientation="vertical" className=""/>
+                    <div className='text-xs text-slate-800 mt-9 text-nowrap'>September 5, 2022</div>
+                <Card date="" title="Card title" description="Most companies try to stay ahead of the curve when it comes to visual design, but for Planetaria we needed to create a brand that would still inspire us 100 years from now when humanity has spread across our entire solar system." />
+                </div>
+                <div className='flex flex-row space-x-4 items-start'>
+                    <Seperator orientation="vertical" className=""/>
+                    <div className='text-xs text-slate-800 mt-9 text-nowrap'>September 5, 2022</div>
+                <Card date="" title="Card title" description="Most companies try to stay ahead of the curve when it comes to visual design, but for Planetaria we needed to create a brand that would still inspire us 100 years from now when humanity has spread across our entire solar system." />
+                </div>
+            </section>
+
+        </div>
+        </div>
+    );
+}
+
+export default page;
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -6,13 +6,13 @@ import Text from "@radui/ui/Text";
 function Card({date, title, description}) {
     return (
         <div className='p-8 space-y-3 hover:bg-gray-400 hover:bg-opacity-70 rounded-2xl max-w-4xl'>
-            <div className='space-x-4 flex flex-row'>
-              
+            {
+                date == "" ? "" :
+                <div className='space-x-4 flex flex-row'>
                     <span className="h-4 w-0.5 rounded-full bg-slate-500 p-0.5"></span>
-
-            <div className='text-xs text-slate-800'>{date}</div>
-            </div>
-            
+                    <div className='text-xs text-slate-800'>{date}</div>
+                </div>
+            }   
             <Heading as="h6" className="text-gray-1000">{title}</Heading>
             <Text className="text-slate-900">{description}</Text>
             <div className='text-teal-900'>Read article</div>
