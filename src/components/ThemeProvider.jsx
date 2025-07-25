86127f777c5dd419eac85457862bf4ccb513ef24
GoldGroove06 <73437174+GoldGroove06@users.noreply.github.com>
2025-07-25 23:03:14 +0530

    changes to local storage

--- src/components/ThemeProvider.jsx
+++ src/components/ThemeProvider.jsx
@@ -1,6 +1,6 @@
 "use client"
 
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import Theme from "@radui/ui/Theme";
 import Navbar from "./Navbar";
 import Layout from "./Layout";
